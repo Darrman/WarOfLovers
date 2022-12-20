6b898090474f72aab1accfc67bbe0ad45197f159
Darrman <30606600+Darrman@users.noreply.github.com>
2022-12-20 17:22:18 +0000

    Created rough Ch1 map By Ch1 I mean FE1 Ch8 cropped down since it's the first chapter.

--- MAKE_HACK_full.cmd
+++ MAKE_HACK_full.cmd
@@ -10,9 +10,9 @@ set "source_rom=%~dp0FE8_clean.gba"
 
 set "main_event=%~dp0ROMBuildfile.event"
 
-set "target_rom=%~dp0SkillsTest.gba"
-set "target_ups=%~dp0SkillsTest.ups"
-set "target_sym=%~dp0SkillsTest.sym"
+set "target_rom=%~dp0GachaJam.gba"
+set "target_ups=%~dp0GachaJam.ups"
+set "target_sym=%~dp0GachaJam.sym"
 
 @rem defining tools
 
