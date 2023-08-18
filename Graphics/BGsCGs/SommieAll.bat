842d9f76e9bd4699b38e439860e9a98f9f679dff
Darrman <30606600+Darrman@users.noreply.github.com>
2023-08-18 23:01:08 +0100

    Added summoning CGs for the original 8
    Right now they're official artwork on a plain background. But they're
    something and they prove the concept.

--- Graphics/BGsCGs/SommieAll.bat
+++ Graphics/BGsCGs/SommieAll.bat
@@ -3,6 +3,6 @@ Setlocal EnableDelayedExpansion
 
 for %%f in (*.png) do (
   set string=%%~nf
-  Sommie.exe !string:~0,3! %%~f %%~nf.dmp %%~nfPal.dmp
+  Sommie.exe !string:~0,3! %%~f bin\%%~nf.dmp bin\%%~nfPal.dmp
 )
 pause
