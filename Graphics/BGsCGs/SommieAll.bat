cbbe7e231a5c5443fce5b8f040a6ed87c0803f54
Darrman <30606600+Darrman@users.noreply.github.com>
2023-09-30 02:23:26 +0100

    Added CGs for all summonable characters
    They aren't in the rom yet; that's for another day.
    Let the record say I have the idea to have Heroes-like text in some late
    scenes, but Marth doesn't need a CG taking up rom space right now.

--- Graphics/BGsCGs/SommieAll.bat
+++ Graphics/BGsCGs/SommieAll.bat
@@ -1,7 +1,7 @@
 @echo off
 Setlocal EnableDelayedExpansion
 
-for %%f in (*.png) do (
+for %%f in (game\*.png) do (
   set string=%%~nf
   Sommie.exe !string:~0,3! %%~f bin\%%~nf.dmp bin\%%~nfPal.dmp
 )
